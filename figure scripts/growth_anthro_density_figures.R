607758cf404ca683f3e4f18d81ffd3d2e92e7d84
Andrew Mertens <amertens@berkeley.edu>
2019-07-26 11:19:34 -0700

    add density plots

--- figure scripts/growth_anthro_density_figures.R
+++ figure scripts/growth_anthro_density_figures.R
@@ -23,7 +23,7 @@ color_levels = c("Change in LAZ\nbetween years 1 and 2", "Change in WLZ\nbetween
 
 
 
-TS_density_plot<-function(Y, X){
+TS_density_plot<-function(Y, X, Xlabel, Ylabel, col_i=1){
   
   dat <- data.frame(Y,X)
   #colnames(dat)[1] <- Ylabel
@@ -33,20 +33,19 @@ TS_density_plot<-function(Y, X){
 
 
   dat$x<-factor(findInterval(dat$X, Acuts))
-  levels(dat$x) = c("Q1","Q2","Q3","Q4")
+  levels(dat$x) = c("Quartile 1","Quartile 2","Quartile 3","Quartile 4")
   
+  Y_color=rep(tableau10[col_i],4)
+
   
-  #TO DO
-  # make color by outcome
-  # but shade by quartile using scale_alpha_manual
-  
-  p <- ggplot(data=dat, aes(x=Y,group=x,color=x,fill=x)) +
+  p <- ggplot(data=dat, aes(x=Y,group=x, fill=x)) +
     facet_wrap(~x,ncol=1) +
-    geom_density(aes(y=..density..),color=NA,alpha=0.7) +
+    geom_density(aes(y=..density.., alpha=x),color=NA) +
+   # scale_alpha_manual(values=c(0.25, 0.5, 0.75, 1),guide=F) +
     #scale_colour_manual(values=tableau10[c(1:4,1:4,1:4,5:7)], drop=TRUE, limits=color_levels) + 
     #scale_fill_manual(values=tableau10[c(1:4,1:4,1:4,5:7)], drop=TRUE, limits=color_levels) + 
-    scale_fill_manual(values=tableau10) + 
-    labs(x=Ylabel, y="Density") +
+    scale_fill_manual(values=Y_color) + 
+    labs(x=Xlabel, y=paste0("Quartile of ",Ylabel)) +
     theme_minimal(base_size=16) +
     theme(legend.position = "none")
   
@@ -56,134 +55,182 @@ TS_density_plot<-function(Y, X){
 
 
 #Hypothesis 1
-h1_delta_laz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_laz_t2_t3, X=d$delta_TS, Ylabel="test")
-h1_delta_waz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_waz_t2_t3, X=d$delta_TS)
-h1_delta_whz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_whz_t2_t3, X=d$delta_TS)
-h1_delta_hcz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_hcz_t2_t3, X=d$delta_TS) 
+h1_delta_laz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_laz_t2_t3, X=d$delta_TS, Ylabel="LAZ change", Xlabel="Change in T/S ratio", col_i=1)
+h1_delta_waz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_waz_t2_t3, X=d$delta_TS, Ylabel="WAZ change", Xlabel="Change in T/S ratio", col_i=3)
+h1_delta_whz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_whz_t2_t3, X=d$delta_TS, Ylabel="WLZ change", Xlabel="Change in T/S ratio", col_i=2)
+h1_delta_hcz_v_delta_ts_dens <- TS_density_plot(Y=d$delta_hcz_t2_t3, X=d$delta_TS, Ylabel="HCZ change", Xlabel="Change in T/S ratio", col_i=4) 
 
 #Hypothesis 2
-h2_len_velocity_v_delta_ts_dens <- TS_density_plot(Y=d$len_velocity_t2_t3, X=d$delta_TS)
-h2_wei_velocity_v_delta_ts_dens <- TS_density_plot(Y=d$wei_velocity_t2_t3, X=d$delta_TS)
-h2_hc_velocity_v_delta_ts_dens <- TS_density_plot(Y=d$hc_velocity_t2_t3, X=d$delta_TS)
+h2_len_velocity_v_delta_ts_dens <- TS_density_plot(Y=d$len_velocity_t2_t3, X=d$delta_TS, Ylabel="Length velocity", Xlabel="Change in T/S ratio", col_i=5)
+h2_wei_velocity_v_delta_ts_dens <- TS_density_plot(Y=d$wei_velocity_t2_t3, X=d$delta_TS, Ylabel="Weight velocity", Xlabel="Change in T/S ratio", col_i=6)
+h2_hc_velocity_v_delta_ts_dens <- TS_density_plot(Y=d$hc_velocity_t2_t3, X=d$delta_TS, Ylabel="Head circumference velocity", Xlabel="Change in T/S ratio", col_i=7)
 
 #Hypothesis 3
-h3_laz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$laz_t3, X=d$delta_TS)
-h3_waz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$waz_t3, X=d$delta_TS)
-h3_whz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$whz_t3, X=d$delta_TS)
-h3_hcz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$hcz_t3, X=d$delta_TS)
+h3_laz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$laz_t3, X=d$delta_TS, Ylabel="Year 2 LAZ", Xlabel="Change in T/S ratio", col_i=1)
+h3_waz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$waz_t3, X=d$delta_TS, Ylabel="Year 2 LAZ", Xlabel="Change in T/S ratio", col_i=3)
+h3_whz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$whz_t3, X=d$delta_TS, Ylabel="Year 2 WLZ", Xlabel="Change in T/S ratio", col_i=2)
+h3_hcz_t3_vs_delta_ts_dens <- TS_density_plot(Y=d$hcz_t3, X=d$delta_TS, Ylabel="Year 2 HCZ", Xlabel="Change in T/S ratio", col_i=4)
 
 #Hypothesis 4
-h4_laz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$laz_t2, X=d$TS_t2)
-h4_waz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$waz_t2, X=d$TS_t2)
-h4_whz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$whz_t2, X=d$TS_t2)
-h4_hcz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$hcz_t2, X=d$TS_t2)
+h4_laz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$laz_t2, X=d$TS_t2, Ylabel="Year 1 LAZ", Xlabel="Year 1 T/S ratio", col_i=1)
+h4_waz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$waz_t2, X=d$TS_t2, Ylabel="Year 1 WAZ", Xlabel="Year 1 T/S ratio", col_i=3)
+h4_whz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$whz_t2, X=d$TS_t2, Ylabel="Year 1 WLZ", Xlabel="Year 1 T/S ratio", col_i=2)
+h4_hcz_t2_vs_ts_t2_dens <- TS_density_plot(Y=d$hcz_t2, X=d$TS_t2, Ylabel="Year 1 HCZ", Xlabel="Year 1 T/S ratio", col_i=4)
 
 #Hypothesis 5
-h5_laz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$laz_t3, X=d$TS_t3)
-h5_waz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$waz_t3, X=d$TS_t3)
-h5_whz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$whz_t3, X=d$TS_t3)
-h5_hcz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$hcz_t3, X=d$TS_t3)
+h5_laz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$laz_t3, X=d$TS_t3, Ylabel="Year 2 LAZ", Xlabel="Year 2 T/S ratio", col_i=1)
+h5_waz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$waz_t3, X=d$TS_t3, Ylabel="Year 2 WAZ", Xlabel="Year 2 T/S ratio", col_i=3)
+h5_whz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$whz_t3, X=d$TS_t3, Ylabel="Year 2 WLZ", Xlabel="Year 2 T/S ratio", col_i=2)
+h5_hcz_t3_vs_ts_t3_dens <- TS_density_plot(Y=d$hcz_t3, X=d$TS_t3, Ylabel="Year 2 HCZ", Xlabel="Year 2 T/S ratio", col_i=4)
 
 #Hypothesis 6
-h6_laz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$laz_t3, X=d$TS_t2)
-h6_waz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$waz_t3, X=d$TS_t2)
-h6_whz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$whz_t3, X=d$TS_t2)
-h6_hcz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$hcz_t3, X=d$TS_t2)
+h6_laz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$laz_t3, X=d$TS_t2, Ylabel="Year 2 LAZ", Xlabel="Year 1 T/S ratio", col_i=1)
+h6_waz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$waz_t3, X=d$TS_t2, Ylabel="Year 2 WAZ", Xlabel="Year 1 T/S ratio", col_i=3)
+h6_whz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$whz_t3, X=d$TS_t2, Ylabel="Year 2 WLZ", Xlabel="Year 1 T/S ratio", col_i=2)
+h6_hcz_t3_vs_ts_t2_dens <- TS_density_plot(Y=d$hcz_t3, X=d$TS_t2, Ylabel="Year 2 HCZ", Xlabel="Year 1 T/S ratio", col_i=4)
 
 #Hypothesis 7
-h7_len_veloc_vs_ts_t2_dens <- TS_density_plot(Y=d$len_velocity_t2_t3, X=d$TS_t2)
-h7_wei_veloc_vs_ts_t2_dens <- TS_density_plot(Y=d$wei_velocity_t2_t3, X=d$TS_t2)
-h7_hc_veloc_vs_ts_t2_dens <- TS_density_plot(Y=d$hc_velocity_t2_t3, X=d$TS_t2)
+h7_len_veloc_vs_ts_t2_dens <- TS_density_plot(Y=d$len_velocity_t2_t3, X=d$TS_t2, Ylabel="Length velocity", Xlabel="Year 1 T/S ratio", col_i=5)
+h7_wei_veloc_vs_ts_t2_dens <- TS_density_plot(Y=d$wei_velocity_t2_t3, X=d$TS_t2, Ylabel="Weight velocity", Xlabel="Year 1 T/S ratio", col_i=6)
+h7_hc_veloc_vs_ts_t2_dens <- TS_density_plot(Y=d$hc_velocity_t2_t3, X=d$TS_t2, Ylabel="Head circumference velocity", Xlabel="Year 1 T/S ratio", col_i=7)
 
 #Hypothesis 8
-h8_delta_laz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_laz_t2_t3, X=d$TS_t2)
-h8_delta_waz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_waz_t2_t3, X=d$TS_t2)
-h8_delta_whz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_whz_t2_t3, X=d$TS_t2)
-h8_delta_hcz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_hcz_t2_t3, X=d$TS_t2)
+h8_delta_laz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_laz_t2_t3, X=d$TS_t2, Ylabel="LAZ change", Xlabel="Year 1 T/S ratio", col_i=1)
+h8_delta_waz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_waz_t2_t3, X=d$TS_t2, Ylabel="WAZ change", Xlabel="Year 1 T/S ratio", col_i=3)
+h8_delta_whz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_whz_t2_t3, X=d$TS_t2, Ylabel="WLZ change", Xlabel="Year 1 T/S ratio", col_i=2)
+h8_delta_hcz_v_ts_t2_dens <- TS_density_plot(Y=d$delta_hcz_t2_t3, X=d$TS_t2, Ylabel="HCZ change", Xlabel="Year 1 T/S ratio", col_i=4)
 
 
 
 #Save density plots
-# save(
-#   #Hypothesis 1
-#   h1_delta_laz_v_delta_ts_dens,
-#   h1_delta_waz_v_delta_ts_dens, 
-#   h1_delta_whz_v_delta_ts_dens,
-#   h1_delta_hcz_v_delta_ts_dens,
-#   #Hypothesis 2
-#   h2_len_velocity_v_delta_ts_dens,
-#   h2_wei_velocity_v_delta_ts_dens,
-#   h2_hc_velocity_v_delta_ts_dens,
-#   #Hypothesis 3
-#   h3_laz_t3_vs_delta_ts_dens, 
-#   h3_waz_t3_vs_delta_ts_dens,
-#   h3_whz_t3_vs_delta_ts_dens, 
-#   h3_hcz_t3_vs_delta_ts_dens, 
-#   #Hypothesis 4
-#   h4_laz_t2_vs_ts_t2_dens, 
-#   h4_waz_t2_vs_ts_t2_dens,
-#   h4_whz_t2_vs_ts_t2_dens, 
-#   h4_hcz_t2_vs_ts_t2_dens, 
-#   #Hypothesis 5
-#   h5_laz_t3_vs_ts_t3_dens, 
-#   h5_waz_t3_vs_ts_t3_dens, 
-#   h5_whz_t3_vs_ts_t3_dens, 
-#   h5_hcz_t3_vs_ts_t3_dens, 
-#   #Hypothesis 6
-#   h6_laz_t3_vs_ts_t2_dens, 
-#   h6_waz_t3_vs_ts_t2_dens, 
-#   h6_whz_t3_vs_ts_t2_dens, 
-#   h6_hcz_t3_vs_ts_t2_dens, 
-#   #Hypothesis 7
-#   h7_len_veloc_vs_ts_t2_dens, 
-#   h7_wei_veloc_vs_ts_t2_dens, 
-#   h7_hc_veloc_vs_ts_t2_dens,
-#   #Hypothesis 8
-#   h8_delta_laz_v_ts_t2_dens,
-#   h8_delta_waz_v_ts_t2_dens,
-#   h8_delta_whz_v_ts_t2_dens, 
-#   h8_delta_hcz_v_ts_t2_dens, 
-#   file=here("/audrie results/telo_growth_spline_fits.Rdata"))
+save(
+  #Hypothesis 1
+  h1_delta_laz_v_delta_ts_dens,
+  h1_delta_waz_v_delta_ts_dens,
+  h1_delta_whz_v_delta_ts_dens,
+  h1_delta_hcz_v_delta_ts_dens,
+  #Hypothesis 2
+  h2_len_velocity_v_delta_ts_dens,
+  h2_wei_velocity_v_delta_ts_dens,
+  h2_hc_velocity_v_delta_ts_dens,
+  #Hypothesis 3
+  h3_laz_t3_vs_delta_ts_dens,
+  h3_waz_t3_vs_delta_ts_dens,
+  h3_whz_t3_vs_delta_ts_dens,
+  h3_hcz_t3_vs_delta_ts_dens,
+  #Hypothesis 4
+  h4_laz_t2_vs_ts_t2_dens,
+  h4_waz_t2_vs_ts_t2_dens,
+  h4_whz_t2_vs_ts_t2_dens,
+  h4_hcz_t2_vs_ts_t2_dens,
+  #Hypothesis 5
+  h5_laz_t3_vs_ts_t3_dens,
+  h5_waz_t3_vs_ts_t3_dens,
+  h5_whz_t3_vs_ts_t3_dens,
+  h5_hcz_t3_vs_ts_t3_dens,
+  #Hypothesis 6
+  h6_laz_t3_vs_ts_t2_dens,
+  h6_waz_t3_vs_ts_t2_dens,
+  h6_whz_t3_vs_ts_t2_dens,
+  h6_hcz_t3_vs_ts_t2_dens,
+  #Hypothesis 7
+  h7_len_veloc_vs_ts_t2_dens,
+  h7_wei_veloc_vs_ts_t2_dens,
+  h7_hc_veloc_vs_ts_t2_dens,
+  #Hypothesis 8
+  h8_delta_laz_v_ts_t2_dens,
+  h8_delta_waz_v_ts_t2_dens,
+  h8_delta_whz_v_ts_t2_dens,
+  h8_delta_hcz_v_ts_t2_dens,
+  file=here("/audrie results/telo_density_plot_objects.Rdata"))
 
 
+  p1 <- plot_grid(
+  h1_delta_laz_v_delta_ts_dens,
+  h1_delta_waz_v_delta_ts_dens,
+  h1_delta_whz_v_delta_ts_dens,
+  h1_delta_hcz_v_delta_ts_dens,  nrow=1, labels = c("","","",""))
+  p2 <- plot_grid(
+  h2_len_velocity_v_delta_ts_dens,
+  h2_wei_velocity_v_delta_ts_dens,
+  h2_hc_velocity_v_delta_ts_dens, nrow=1, labels = c("","",""))
+  p3 <- plot_grid(
+  h3_laz_t3_vs_delta_ts_dens,
+  h3_waz_t3_vs_delta_ts_dens,
+  h3_whz_t3_vs_delta_ts_dens,
+  h3_hcz_t3_vs_delta_ts_dens, nrow=1, labels = c("","","",""))
+  p4 <- plot_grid(
+  h4_laz_t2_vs_ts_t2_dens,
+  h4_waz_t2_vs_ts_t2_dens,
+  h4_whz_t2_vs_ts_t2_dens,
+  h4_hcz_t2_vs_ts_t2_dens, nrow=1, labels = c("","","",""))
+  p5 <- plot_grid(
+  h5_laz_t3_vs_ts_t3_dens,
+  h5_waz_t3_vs_ts_t3_dens,
+  h5_whz_t3_vs_ts_t3_dens,
+  h5_hcz_t3_vs_ts_t3_dens, nrow=1, labels = c("","","",""))
+  p6 <- plot_grid(
+  h6_laz_t3_vs_ts_t2_dens,
+  h6_waz_t3_vs_ts_t2_dens,
+  h6_whz_t3_vs_ts_t2_dens,
+  h6_hcz_t3_vs_ts_t2_dens, nrow=1, labels = c("","","",""))
+  p7 <- plot_grid(
+  h7_len_veloc_vs_ts_t2_dens,
+  h7_wei_veloc_vs_ts_t2_dens,
+  h7_hc_veloc_vs_ts_t2_dens, nrow=1, labels = c("","",""))
+  p8 <- plot_grid(
+  h8_delta_laz_v_ts_t2_dens,
+  h8_delta_waz_v_ts_t2_dens,
+  h8_delta_whz_v_ts_t2_dens,
+  h8_delta_hcz_v_ts_t2_dens, nrow=1, labels = c("","","",""))
 
 
+p1 <- plot_grid(plist11, plist12, plist13, plist14, nrow=1, labels = c("","","",""))
+p2 <- plot_grid(plist21, plist22, plist23, nrow=1, labels = c("","",""))
+p3 <- plot_grid(plist31, plist32, plist33, plist34, nrow=1, labels = c("","","",""))
+p4 <- plot_grid(plist41, plist42, plist43, plist44, nrow=1, labels = c("","","",""))
+p5 <- plot_grid(plist51, plist52, plist53, plist54, nrow=1, 
+                labels = c("Adjusted differences between quartiles of telomere length at Year 2 for each growth outcome","","",""),
+                hjust=1,vjust=1)
+p6 <- plot_grid(plist61, plist62, plist63, plist64, nrow=1, labels = c("","","",""))
+p7 <- plot_grid(plist71, plist72, plist73, nrow=1, labels = c("","",""))
+p8 <- plot_grid(plist81, plist82, plist83, plist84, nrow=1, labels = c("","","",""))
 
-p <- ggplot(data=d,aes(x=whz,group=agecat,color=agecat,fill=agecat)) +
-  facet_wrap(~agecat,ncol=1) +
-  geom_density(aes(y=..density..),color=NA,alpha=0.7)+
-  geom_vline(aes(xintercept= -2), size=2) +
-  scale_x_continuous(limits = c(-5,5), breaks = c(-4:4)) +
-  scale_fill_manual(values=rep(pcols[4],10)) +
-  scale_color_manual(values=rep(pcols[4],10))  +
-  labs(x="WLZ",y="Density") +
-  theme_minimal(base_size=16) +
-  theme(legend.position = "none") +
-  ggtitle("WLZ distribution by child age")
+ggsave(p1, file = here("figures/density plots/telo-density_h1.png"), height=6, width=14)
+ggsave(p2, file = here("figures/density plots/telo-density_h2.png"), height=6, width=14)
+ggsave(p3, file = here("figures/density plots/telo-density_h3.png"), height=6, width=14)
+ggsave(p4, file = here("figures/density plots/telo-density_h4.png"), height=6, width=14)
+ggsave(p5, file = here("figures/density plots/telo-density_h5.png"), height=6, width=14)
+ggsave(p6, file = here("figures/density plots/telo-density_h6.png"), height=6, width=14)
+ggsave(p7, file = here("figures/density plots/telo-density_h7.png"), height=6, width=14)
+ggsave(p8, file = here("figures/density plots/telo-density_h8.png"), height=6, width=14)
+
+# #Adjusted differences between quartiles of change in telomere length between Years 1 and 2 for each growth outcome
+# pcomb1 <- plot_grid(p1,
+#                     p2,
+#                     p3,
+#                     ncol=1,
+#                     labels = c("","",""),
+#                     hjust=0.5, vjust=0.5,
+#                     rel_heights = c(1, 1, 1))
+# #Adjusted differences between quartiles of telomere length at Year 1 for each growth outcome
+# pcomb2 <- plot_grid(p4,
+#                     p6,
+#                     p8,
+#                     p7,
+#                     ncol=1,
+#                     labels = c("","","",""),
+#                     hjust=0.5,vjust=0.5,
+#                     rel_heights = c(1, 1, 1, 1))
+# 
+# 
+# 
+# 
+# ggsave(pcomb1, file = here("figures/telo-growth-splines_1.png"), height=12, width=14)
+# ggsave(pcomb2, file = here("figures/telo-growth-splines_2.png"), height=16, width=14)
+# ggsave(p5, file = here("figures/telo-growth-splines_3.png"), height=4, width=14)
 
 
-#Color by region
-df <- d %>% filter(agecat %in% c("Birth","3 months","6 months","12 months","18 months","24 months"))
-df$agecat <- gsub("months", "mo.", df$agecat)
-df$agecat <- factor(df$agecat, levels=c("Birth","3 mo.","6 mo.","12 mo.","18 mo.","24 mo."))
-df$region <- factor(df$region, levels=c("Asia","Africa","Latin America"))
-
-df <- df %>% filter(studyid!="TanzaniaChild2")
-
-p <- ggplot(data=df,aes(x=whz,group=region,color=region,fill=region)) +
-  #facet_wrap(~agecat,ncol=1) +
-  facet_grid(agecat~.,scales='free_y',space='free_y') +
-  geom_density(aes(y=..density..),color=NA,alpha=0.5)+
-  geom_vline(aes(xintercept= -2), size=2) +
-  geom_vline(aes(xintercept= meanWLZ, group=region, color=region), size=2) +
-  scale_x_continuous(limits = c(-4,4), breaks = c(-4:4)) +
-  scale_fill_manual(values=rep(pcols,10)) +
-  scale_color_manual(values=rep(pcols,10))  +
-  labs(x="WLZ",y="Density") +
-  theme_minimal(base_size=16) +
-  theme(legend.position = "none") +
-  ggtitle("WLZ distribution by child age and region")
-p
 
 
 
