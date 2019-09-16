50f220f26a2b1ba54ee2f28261cc74ab112602b5
Sophia Tan <sophiatan@berkeley.edu>
2019-09-15 18:28:41 -0700

    completed supp table 8 with csv and html

--- table scripts/MIS09 Supplementary Tables/MIS09-TableS8.R
+++ table scripts/MIS09 Supplementary Tables/MIS09-TableS8.R
@@ -1,12 +1,13 @@
 rm(list=ls())
+library("xtable")
 source(here::here("0-config.R"))
-source(here('table scripts/MIS09-Table6.R'))
+source(here('table scripts/MIS09-immune-Table6.R'))
 
 load(here("audrie results/immune_ipcw.RData"))
 
 maketblvalue<-function(ipcwadjvar){
   rounded<-round(ipcwadjvar, 2)
-  paste(rounded[1], "(", rounded[3], ", ", rounded[4], ")", sep="")
+  paste(rounded[1], " (", rounded[3], ", ", rounded[4], ")", sep="")
 }
 
 ipcws8<-c(" ", " ", maketblvalue(d23_il1_adj_ipcw_L$`unlist(d23_il1_adj_ipcw$estimates$ATE)`),
@@ -25,8 +26,9 @@ ipcws8<-c(" ", " ", maketblvalue(d23_il1_adj_ipcw_L$`unlist(d23_il1_adj_ipcw$est
           " ", " ", maketblvalue(d23_igf_adj_ipcw_L$`unlist(d23_igf_adj_ipcw$estimates$ATE)`))
 
 tbls8<-cbind(tbl6, ipcws8)
-#once absolute mean column is added to table 6 change index from 8 to 9
-names(tbls8)[8]<-"IPCW adjusted difference: Intervention vs. Control (95% CI)"
+names(tbls8)[9]<-"IPCW adjusted difference: Intervention vs. Control (95% CI)"
 
-write.csv(tbls8, file=here('tables/miso9-supptable8.csv'))
+write.csv(tbls8, file=here('tables/miso9-immune-supptable8.csv'))
+print(xtable(tbls8), type="html", file=here("tables/miso9-immune-supptable8.html"))
+
  
