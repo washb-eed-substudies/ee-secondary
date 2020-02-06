9a970ad273aa2178632ba9cd8a443c9be3d67707
audriel <audrielin@berkeley.edu>
2020-02-05 16:22:26 -0800

    changed covariates of growth --> TL analysis

--- audrie R scripts/observational/quartileTMLE_telo_growth.R
+++ audrie R scripts/observational/quartileTMLE_telo_growth.R
@@ -837,11 +837,10 @@ Wvars<-c("sex","birthord", "momage", "momheight","momedu",
          "floor", "walls", "elec", "asset_wardrobe", "asset_table", "asset_chair", "asset_clock", "asset_khat", 
          "asset_chouki", "asset_radio", "asset_tv", "asset_refrig",
          "asset_bike", "asset_moto", "asset_sewmach", "asset_mobile", 
-         "n_cattle", "n_goat", "n_chicken", "monsoon_ht2", "monsoon_ht3", "ageday_ht2", 
+         "n_cattle", "n_goat", "n_chicken", "monsoon_ht2", "monsoon_ht3", "ageday_ht2",
          "ageday_ht3", "tr", "cesd_sum_t2", "cesd_sum_ee_t3", "pss_sum_mom_t3", "diar7d_t2", "diar7d_t3", 
-         "life_viol_any_t3")
+         "life_viol_any_t3", "lenhei_med_t1", "weight_med_t1")
 
-#add "lenhei_med_t1", "weight_med_t1" to covariate list?
 
 h6badj.res <- tmle_quart(dat=d, 
                         Y="TS_t3", 
