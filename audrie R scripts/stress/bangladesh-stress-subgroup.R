1ef8ce075e937735a657a47050e602788ef12e05
Zach <zbutzin@gmail.com>
2020-06-09 13:05:21 -0700

    Error washb_function stress subgroup analysis

--- audrie R scripts/stress/bangladesh-stress-subgroup.R
+++ audrie R scripts/stress/bangladesh-stress-subgroup.R
@@ -61,7 +61,6 @@ SL.library=c("SL.mean","SL.glm","SL.bayesglm","SL.gam","SL.glmnet")
 # sort the data for perfect replication with andrew on the V-fold cross-validation
 d <- d[order(d$block,d$clusterid,d$dataid,d$childid),]
 
-
 ###################
 #Subgroup Analyses @T2
 ###################
@@ -97,7 +96,23 @@ washb_function <- function(df,x) {
 }
 
 #grab the variables with prefix 't2_' from the data frame and then apply the washb_function
+
+#####ERROR HERE####
 list_stress <- lapply(names(d)[grep('t2_', names(d))],  function(x) washb_function(d,x))
+
+list_stress
+
+#Troubleshoot - change d to df
+
+list_stress <- lapply(names(df)[grep('t2_', names(df))], washb_function(df,W$sex))
+
+list_stress
+
+#create new df with subset to t2
+
+df_t2 <- df[,c("t2_ipf2a3", "t2_23dinor", "t2_ipf2a6", "t2_812iso")]
+
+list_stress2 <- function(x) washb_function(df_t2,x)
 
 list_stress
 
@@ -136,6 +151,8 @@ d$tr <- factor(d$tr,levels=c("Control","Nutrition + WSH"))
 
 
 # subset to columns needed for unadjusted 
+
+#### Should this be same as df defined above for T2? #####
 df = d[,c("block", "tr","t2_ipf2a3", "t2_23dinor", "t2_ipf2a6", "t2_812iso", "t3_pre_saa", "t3_pre_cort",
           "t3_post_saa", "t3_post_cort", "t3_sys", "t3_dia", "t3_heart", "t3_nr3c1", "t3_cpg12")]
 df$block=as.factor(df$block)
@@ -201,6 +218,9 @@ list_stress
 
 
 #to save each matrix separately for comparing with Andrew. 
+
+####These two seem to be immune (rather than stress) outcomes, but I am unsure how to ammend without the list_stress output
+
 t3_igf_subgroup_L<-list_stress$t3_ln_igf
 
 
