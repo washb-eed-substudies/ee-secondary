6e4e3bf8c5f59957a3410f19083a3e2200b3b79a
geneh0 <52678798+geneh0@users.noreply.github.com>
2020-08-12 10:52:18 -0700

    maternal cortisol replication script

--- maternal cortisol/maternal cortisol replication.R
+++ maternal cortisol/maternal cortisol replication.R
@@ -1,7 +1,27 @@
 ## maternal cortisol replication ##
 
+library(tidyverse)
+
 rm(list=ls())
 source(here::here("0-config.R"))
 
-gene <- read.csv(here('maternal cortisol/Gene/maternal-cortisol.csv'))
-soph <- read.csv(here('maternal cortisol/Sophia scripts/cleaned_maternal_cortisol_ST.csv'))
+gene <- read.csv(here('maternal cortisol/Gene/maternal-cortisol.csv')) %>% 
+  select(-X) %>% 
+  rename(lab.tech = lab_tech) %>% 
+  drop_na('sample')
+
+soph <- read.csv(here('maternal cortisol/Sophia scripts/cleaned_maternal_cortisol_ST.csv')) %>% 
+  select(-X)
+
+`%notin%` <- Negate(`%in%`)
+colnames(gene)[colnames(gene) %notin% colnames(soph)]
+colnames(soph)[colnames(soph) %notin% colnames(gene)]
+
+
+gene %>% 
+  filter(run == 3) %>% 
+  View()
+
+gene$sample[gene$sample %notin% soph$sample]
+soph$sample[soph$sample %notin% gene$sample]
+
