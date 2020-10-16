e9dce3dc132a044aab65070c6e94c812883b0a99
Andrew Mertens <amertens@berkeley.edu>
2020-10-16 14:42:14 -0700

    update age checking

--- gene scripts/child_age.R
+++ gene scripts/child_age.R
@@ -29,13 +29,13 @@ dev <- dev %>%
 
 # select columns from eed data
 stool <- stool %>% 
-  select("childid", 'hhid', 'clusterid', "aat2", "mpo2", "neo2", "stool_ml_date", 'DOB')
+  select("childid", 'hhid', 'clusterid', "aat2", "mpo2", "neo2", "stool_ml_date", "stool_el_date" ,'DOB')
 
 stool$DOB <- ymd(stool$DOB)
 stool$urine_ml_date <- ymd(stool$stool_ml_date)
 
 urine <- urine %>% 
-  select("childid", "hhid", 'clusterid',"Mann2", "Lact2", 'LM2', 'urine_ml_date', 'DOB')
+  select("childid", "hhid", 'clusterid',"Mann2", "Lact2", 'LM2', 'urine_ml_date', "urine_el_date", 'DOB')
 
 urine$DOB <- ymd(urine$DOB)
 urine$urine_ml_date <- ymd(urine$urine_ml_date)
@@ -64,8 +64,8 @@ dev <- dev %>%
   )
 
 # % of children with development data collected after midline
-mean(motor$age_midline)
-mean(dev$age_midline, na.rm = TRUE)
+mean(motor$age_midline_m)
+mean(dev$age_midline_d, na.rm = TRUE)
 
 
 #######################################################
@@ -73,18 +73,29 @@ mean(dev$age_midline, na.rm = TRUE)
 #######################################################
 
 urine$age_urine_ml <- interval(urine$DOB, urine$urine_ml_date) / months(1)
-stool$age_urine_ml <- interval(stool$DOB, stool$stool_ml_date) / months(1)
+stool$age_stool_ml <- interval(stool$DOB, stool$stool_ml_date) / months(1)
 
+urine$age_urine_el <- interval(urine$DOB, urine$urine_el_date) / months(1)
+stool$age_stool_el <- interval(stool$DOB, stool$stool_el_date) / months(1)
+
+summary(urine$age_urine_ml)
+summary(stool$age_urine_ml)
 
 #######################################################
 # merge datasets
 #######################################################
 
-biomarkers <- full_join(stool, urine)
+dim(stool)
+dim(urine)
+biomarkers <- full_join(stool, urine, by=c("childid","DOB"))
+dim(biomarkers)
 
-devel <- full_join(motor, dev)
+dim(motor)
+dim(dev)
+devel <- full_join(motor, dev, by=c("childid"))
+dim(devel)
 
-all_dta <- full_join(biomarkers, devel)
+all_dta <- full_join(biomarkers, devel, by=c("childid"))
 
 
 #######################################################
@@ -113,5 +124,14 @@ all_dta <- all_dta %>%
 
 sum(all_dta$childage_devmm > all_dta$age_urine_ml)
 
+summary(all_dta$childage_devmm - all_dta$age_urine_ml)
+summary(all_dta$childage_devmm)
+summary(all_dta$age_urine_ml)
 
 
+all_dta$childage_dev <- all_dta$childage_dev/30.4167
+summary(all_dta$age_urine_el)
+summary(all_dta$childage_dev)
+
+
+summary(all_dta$childage_dev - all_dta$age_urine_el)
